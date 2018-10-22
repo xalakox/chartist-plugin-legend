ca0c723cc3623aa4365c637d850397f9b6816dd5
Salvador Aceves <salvador@aceves.mx>
2018-10-22 00:09:33 -0500

    under es6 import babel schema root would not exist

--- chartist-plugin-legend.js
+++ chartist-plugin-legend.js
@@ -1,5 +1,5 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (root && typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['chartist'], function (chartist) {
             return (root.returnExportsGlobal = factory(chartist));
